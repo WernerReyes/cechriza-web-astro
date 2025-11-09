e024d4afca73dd1d1530387f14d6447efdce3b3b
Werner Reyes <reyespilcowerner@gmail.com>
2025-11-09 15:29:49 -0500

    feat: load environment variables for site configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import vercel from '@astrojs/vercel';
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import sitemap from '@astrojs/sitemap';
+import { loadEnv } from 'vite';
+
+const { URL_SITE } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server', // 👈 genera SSR
   // adapter: node({ mode: 'standalone' }),
-  site: process.env.URL_SITE,
+  site: URL_SITE,
   adapter: vercel({}),
   redirects: {
     '/': '/home',
